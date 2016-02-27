7089174dba198d5254ddf746009d589ac2fe8689
Mikael Hellman <mikael.hellman@gmail.com>
2016-02-27 09:19:38 +0100

    Removed test put

--- js/main.js
+++ js/main.js
@@ -7,14 +7,6 @@ var log = function (out) {
     console.log(out)
 };
 
-var put = function (out) {
-    log(out);
-    var pre = document.createElement("p");
-    pre.style.wordWrap = "break-word";
-    pre.innerHTML = out;
-    output.appendChild(pre);
-};
-
 requirejs.onError = function (err) {
     log(err.requireType);
     log(err.requireModules);
