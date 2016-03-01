42c079244349a98c1b3a7f8a2d89763a6d389c54
Mikael Hellman <mikael.hellman@gmail.com>
2016-03-01 12:55:30 +0100

    Refactoring

--- js/app/3d/Behaviours/BeastBehaviour.js
+++ js/app/3d/Behaviours/BeastBehaviour.js
@@ -23,15 +23,37 @@ define([
         timeFraction = obj.calcTimeFraction(tickCount, timeFraction);
         obj.node.rotation.z -= (Math.random() * (0.019) + 0.015);
         if (timeFraction < 0.5) {
-          var scale = 1 - timeFraction;
-          obj.node.scale.x = Math.max(0.75, scale);
-          obj.node.scale.y = Math.max(0.75, scale);
+          __scaleDown(obj, timeFraction);
         } else {
-          obj.node.scale.x = Math.min(1.0, timeFraction + 0.25);
-          obj.node.scale.y = Math.min(1.0, timeFraction + 0.25);
+          __scaleUp(obj, timeFraction);
         }
       }
     };
+
+    /// INTERNAL ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Scale down animation
+     * @param obj
+     * @param timeFraction
+     * @private
+     */
+    function __scaleDown(obj, timeFraction) {
+      var scale = 1 - timeFraction;
+      obj.node.scale.x = Math.max(0.75, scale);
+      obj.node.scale.y = Math.max(0.75, scale);
+    }
+
+    /**
+     * Scale up animation
+     * @param obj
+     * @param timeFraction
+     * @private
+     */
+    function __scaleUp(obj, timeFraction) {
+      obj.node.scale.x = Math.min(1.0, timeFraction + 0.25);
+      obj.node.scale.y = Math.min(1.0, timeFraction + 0.25);
+    }
 
     // Return Object
     return BeastBehaviour;
